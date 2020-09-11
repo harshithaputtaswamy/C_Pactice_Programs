9cf3820b83ddf5135dc59804de5a08c8e5eea7ce
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2020-09-11 18:53:54 +0530

    Create Tic_tac_toe.c

---
+++ Tic_tac_toe.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+
+
+//Declaring global variables
+char square[10] = {'0','1','2','3','4','5','6','7','8','9'};
+int choice,player;
+
+int checkForWin();
+void boardDisplay();
+void markBoard(char mark);
+
+
+int main()
+{
+    int gameStatus = 1;
+    char mark;
+     player = 1;
+    do{
+         boardDisplay();
+         player = (player % 2)? 1 : 2 ;
+         printf("Player %d, enter a number : ",player);
+         scanf("%d",&choice);
+         mark = (player == 1)? 'X' : 'O' ;
+         markBoard(mark);
+         gameStatus = checkForWin();
+         player++;
+
+    }
+   while(gameStatus == -1);
+    if (gameStatus == 1)
+        printf("Player %d is the winner!!",--player);
+    else
+        printf("It's a draw.");
+
+
+    return 0;
+}
+    /****
+    FUNCTION TO RETURN GAME STATUS
+    1 FOR GAME OVER AND ONE OF THEM WON THE GAME
+    0 FOR GAME IS OVER WITH NO WINNER
+    -1 FOR GAME NOT YET FINISHED
+    ****/
+
+    int checkForWin()
+    {
+        int returnValue = 0;
+
+         if ( square[1] == square[2] && square[2]== square[3])
+            returnValue = 1;
+
+        else if ( square[4] == square[5] && square[5] == square[6])
+            returnValue = 1;
+
+        else  if ( square[7] == square[8] && square[8] == square[9])
+            returnValue = 1;
+
+        else  if ( square[1] == square[5] && square[5] == square[9])
+            returnValue = 1;
+
+        else  if ( square[3] == square[5] && square[5] == square[7])
+            returnValue = 1;
+
+        else  if ( square[1] == square[4] && square[4] == square[7])
+            returnValue = 1;
+
+        else  if ( square[2] == square[5] && square[5] == square[8])
+            returnValue = 1;
+
+        else  if ( square[3] == square[6] && square[6] == square[9])
+            returnValue = 1;
+
+        else if ( square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9'  )
+           returnValue = 0;
+           else
+            returnValue = -1;
+         return returnValue;
+    }
+
+   //programming for display of the game
+   void boardDisplay()
+    {
+        system("cls");
+        printf("\n\n\n             TIC TAC TOE\n\n\n\n\n");
+        printf("Player 1 (X)           Player 2 (O)\n\n\n\n\n");
+
+        printf("     |     |     \n");
+        printf("  %c  |  %c  |  %c  \n",square[1],square[2],square[3]);
+        printf("_____|_____|_____\n");
+
+        printf("     |     |     \n");
+        printf("  %c  |  %c  |  %c  \n",square[4],square[5],square[6]);
+        printf("_____|_____|_____\n");
+
+        printf("     |     |     \n");
+        printf("  %c  |  %c  |  %c  \n",square[7],square[8],square[9]);
+        printf("     |     |     \n");
+
+    }
+
+
+   void markBoard(char mark)
+    {
+        if ( choice == 1 && square[1] == '1')
+        square[1] = mark;
+
+        else if ( choice == 2 && square[2] == '2')
+        square[2] = mark;
+
+        else if ( choice == 3 && square[3] == '3')
+        square[3] = mark;
+
+        else if ( choice == 4 && square[4] == '4')
+        square[4] = mark;
+
+        else if ( choice == 5 && square[5] == '5')
+        square[5] = mark;
+
+        else if ( choice == 6 && square[6] == '6')
+        square[6] = mark;
+
+        else if ( choice == 7 && square[7] == '7')
+        square[7] = mark;
+
+        else if ( choice == 8 && square[8] == '8')
+        square[8] = mark;
+
+        else if ( choice == 9 && square[9] == '9')
+        square[9] = mark;
+
+
+        else
+
+       {
+         printf("Invalid move");
+        player--;
+        getch();
+        }
+
+
+    }
+
+
